1fb87c70148f79b1119e50b3b62ebea5ff7cae14
whitehaker <whthaker@gmail.com>
2013-10-06 16:17:40 +0400

    Работа над интерфейсами

--- PayForward/urls.py
+++ PayForward/urls.py
@@ -1,12 +1,16 @@
 from django.conf.urls import patterns, include, url
-
+from tasks.views import *
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'PayForward.views.home', name='home'),
+    url(r'^$', index),
+    url(r'^task/([0-9]+)?/$', task),
+    url(r'^tasks/$',tasks),
+    url(r'^people/$', people),
+    url(r'^profile/([0-9]+)?/$', profile),
     # url(r'^PayForward/', include('PayForward.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
@@ -14,4 +18,9 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+    #develop
+    url(r'^uploads/(?P<path>.*)$','django.views.static.serve',
+        {'document_root':'/home/master/djcode/payforward/uploads'}),
+    url(r'^media/(?P<path>.*)$','django.views.static.serve',
+        {'document_root':'/home/master/djcode/payforward/media'}),
 )
--- tasks/views.py
+++ tasks/views.py
@@ -1,1 +1,17 @@
-# Create your views here.
+from django.shortcuts import render_to_response
+from tasks.models import *
+
+def index(request):
+    return render_to_response('index.html')
+
+def tasks(request):
+    return render_to_response('tasks.html')
+
+def task(request,task_id):
+    return render_to_response('task.html')
+
+def profile(request,user_id):
+    return render_to_response('profile.html')
+
+def people(request):
+    return render_to_response('people.html')
